07a27b62c0758b0030c1d2901d2109244d5e84fd
xavier niquet <niquet_xavier2002@yahoo.fr>
2020-10-04 11:31:48 +0200

    unicafe 6-11

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,42 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-import Header from "./componants/Header"
-import Content from "./componants/Content"
-import Total from "./componants/Total"
+import Header from "./componants/Header";
+import Content from "./componants/Content";
+import Total from "./componants/Total";
 
 const App = () => {
-  const course  = {
-  name : 'Half Stack application development',
-  parts : [{ name : 'Fundamentals of React', exercises: 10 },
-           { name : 'Using props to pass data',exercises : 7},
-           { name : 'State of a component',exercises: 14 }]
-  }
-  
- 
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      { name: "Fundamentals of React", exercises: 10 },
+      { name: "Using props to pass data", exercises: 7 },
+      { name: "State of a component", exercises: 14 },
+    ],
+  };
+
   return (
     <div>
-      <Header course={course.name}/>
-      <Content parts={course.parts}/>
-      
-    
-      <Total total= {course.parts.reduce((acc,obj)=> acc + obj.exercises,0) } />
-    
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total
+        total={course.parts.reduce((acc, obj) => acc + obj.exercises, 0)}
+      />
     </div>
-  )
-}
-
-
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
- 
---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,69 @@
+import React ,{useState} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+const Statistics = ({good,neutral,bad}) => (<>
+<Statistic label="average"  value={ (good- bad )/ (good+neutral+bad)} format=""/>
+<Statistic label="positive" value={100 * good / (good + neutral + bad) } format="%"/>
+</>)
+
+const Statistic = ({label,value,format}) => <tr><td>{label}{value}{format}</td></tr>
+
+const Button = ({label,updateFeedback}) => <button onClick={() => updateFeedback(label)}>{label}</button>
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const handlerButton = (label) => {
+    switch (label) {
+      case "good":
+        setGood(good + 1 )
+        break;
+      case "neutral":
+        setNeutral(neutral + 1 )
+        break;
+      case "bad":
+        setBad(bad + 1 )
+        break;
+      
+      default:
+        break;
+    }
+  }
+  return (
+    <div>
+      <h1>give feedback</h1>
+     
+      <Button label="good" updateFeedback={handlerButton}/>
+      <Button label="neutral" updateFeedback={handlerButton}/>
+      <Button label="bad" updateFeedback={handlerButton}/>
+      <h1>statistics</h1>
+      <table>
+        <tbody>
+          <tr>
+            <td>good</td>
+            <td>{good}</td>
+         </tr>
+         <tr>
+            <td>neutral</td>
+            <td>{neutral}</td>
+          </tr>
+          <tr>
+            <td>bad</td>
+            <td>{bad}</td>
+          </tr>{good === 0 && neutral === 0 && bad === 0 ?<tr><td>No feedback given</td></tr>:<Statistics {...{ good,bad,neutral}}/>
+      } </tbody>
+      </table>
+    </div>
+  )
+}
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+ 

e2287058d78ce957e50565bde6a740c08bff82e8
xavier niquet <niquet_xavier2002@yahoo.fr>
2020-10-05 16:06:59 +0200

    phonebook 2 11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,19 @@
-import React, { useState } from "react";
-
+import React, { useEffect, useState } from "react";
+import axios from "axios"
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 
+ 
+
 const App = () => {
-  const [persons, setPersons] = useState(() => [
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
 
   const [filter, setFilter] = useState("");
+  useEffect(()=>{
+    axios.get("http://localhost:3001/persons")
+    .then(({data})=> setPersons(data))
+  },[] )
 
   const handlerNewPerson = (person) => {
     if (
@@ -43,4 +44,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default App
